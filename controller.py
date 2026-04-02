0c7cda1acb38a5f9c684c4b391d4939af08204eb
Abhay Thapa <Abhay.Thapa@usm.edu>
2026-04-02 12:00:23 -0500

    added input
    added input for number of people

--- controller.py
+++ controller.py
@@ -9,6 +9,9 @@ class TipCalculator(BoxLayout):
     bill_input = StringProperty()
     tip_output = StringProperty()
     
+    num_people_input=StringProperty()
+    split_output=StringProperty()
+    
     __model: TipModel
 
     def __init__(self, model):
@@ -19,9 +22,19 @@ class TipCalculator(BoxLayout):
         try:
             self.__model.bill_amount = float(self.bill_input)
             self.__model.tip_percent = float(tip_percent)
-            
+            # Get tip amount from model
+            tip_amount = self.__model.tip_amount
             self.tip_output = f"Tip Amount: ${self.__model.tip_amount:.2f}"
+            try:
+                people = int(self.num_people_input)
+                if people <= 0:
+                    people = 1
+            except ValueError:
+                people = 1
 
+        # Update outputs
+            
+            self.split_output = f"Each Person Pays: ${(self.__model.bill_amount + tip_amount)/people:.2f}"
         except ValueError as ex:
             print("error")
         
