233c58f157cd5ac8d4fa19ed8edcad4ebfbea2c8
prakash5910 <prakashlamsal111@gmail.com>
2026-04-02 12:02:46 -0500

    Update controller.py
    updated

--- controller.py
+++ controller.py
@@ -8,9 +8,8 @@ Builder.load_file('view.kv')
 class TipCalculator(BoxLayout):
     bill_input = StringProperty()
     tip_output = StringProperty()
-    
-    num_people_input=StringProperty()
-    split_output=StringProperty()
+    num_people_input = StringProperty()
+    split_output = StringProperty()
     
     __model: TipModel
 
@@ -22,19 +21,9 @@ class TipCalculator(BoxLayout):
         try:
             self.__model.bill_amount = float(self.bill_input)
             self.__model.tip_percent = float(tip_percent)
-            # Get tip amount from model
-            tip_amount = self.__model.tip_amount
+            self.__model.num_people = int(self.num_people_input)
             self.tip_output = f"Tip Amount: ${self.__model.tip_amount:.2f}"
-            try:
-                people = int(self.num_people_input)
-                if people <= 0:
-                    people = 1
-            except ValueError:
-                people = 1
-
-        # Update outputs
-            
-            self.split_output = f"Each Person Pays: ${(self.__model.bill_amount + tip_amount)/people:.2f}"
+            self.split_output = f"Per Person: ${self.__model.total_per_person:.2f}"
         except ValueError as ex:
             print("error")
         
